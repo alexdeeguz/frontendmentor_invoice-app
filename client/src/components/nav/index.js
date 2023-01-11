2f0da344edfc268ff91c7b152e64185ebe0ee87f
alex <alexdg316@gmail.com>
2023-01-11 09:18:01 -0800

    Create nav component

---
+++ client/src/components/nav/index.js
@@ -0,0 +1,19 @@
+import './nav.css'
+
+const Nav = () => {
+    return (
+        <nav>
+            <div className='logo'>
+                <img src="/assets/logo.svg" alt="logo" />
+                <div className='logo__bottom'></div>
+            </div>
+            <div className='nav__right'>
+                <img src='/assets/icon-moon.svg' alt='theme icon'/>
+                <div className='nav__divider'></div>
+                <img src='/assets/image-avatar.jpg' />
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
