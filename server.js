6229e32d6dfc4ad63ef2ecab9509203e5649a47a
alex <alexdg316@gmail.com>
2023-01-16 19:24:44 -0800

    Prep for production

--- server.js
+++ server.js
@@ -15,3 +15,13 @@ app.use("/api/invoices", require("./routes/api/invoices"));
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
