0ed4b8d5026397de97088f39980410836ce135d3
alex <alexdg316@gmail.com>
2023-01-16 20:00:35 -0800

    Revert

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: __dirname + "/.env" });
-}
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
